4d64d518df2b97efdd872ba571910915e098181c
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2025-05-19 19:17:05 +0200

    feat: improve scroll responsiveness

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -112,7 +112,7 @@ export default function Chat() {
 
   return (
     <AuthGuard>
-      <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <div className="flex min-h-svh lg:h-svh w-full items-center justify-center p-6 md:p-10">
         <div className="w-full max-w-9/10 lg:max-w-5/10">
           <div className="flex flex-col gap-6">
             <Card>
